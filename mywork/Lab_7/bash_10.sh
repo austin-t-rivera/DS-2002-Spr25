3f4d6a514bb9bfb6b2d797260feacce7227e0f54
Kylee L <kyleesb27@gmail.com>
2025-04-13 10:12:09 -0400

    updating

--- mywork/Lab_7/bash_10.sh
+++ mywork/Lab_7/bash_10.sh
@@ -6,34 +6,22 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-# Assign input arguments to variables
+# Assigning args to vars
 FILE=$1
 BUCKET_NAME=$2
 EXPIRATION=$3
 
-# Check if the file exists
-if [ ! -f "$FILE" ]; then
-    echo "Error: File '$FILE' not found."
-    exit 2
-fi
-
-# Upload the file to the S3 bucket 
+# Uploading file to bucket
 aws s3 cp "$FILE" "s3://$BUCKET_NAME/"
 
-# Check if upload was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to upload file to S3 bucket."
-    exit 3
-fi
-
-# Generate a pre-signed URL that expires in the given expiration time (default: 604800 seconds)
+# Generating a URL
 URL=$(aws s3 presign "s3://$BUCKET_NAME/$(basename "$FILE")" --expires-in "$EXPIRATION")
 
-# Check if the URL generation was successful
+# result
 if [ $? -eq 0 ]; then
     echo "File uploaded successfully!"
-    echo "Pre-signed URL (expires in $EXPIRATION seconds): $URL"
+    echo "Pre-signed URL: $URL"
 else
     echo "Error: Failed to generate pre-signed URL."
-    exit 4
+    exit 2
 fi
