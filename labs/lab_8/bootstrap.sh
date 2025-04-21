4f3bd0d981e6f59f828250822363dd43012504ee
ayshahussen <eku2ec@virginia.edu>
2025-04-20 23:51:12 -0400

    fixes to bootstrapping script

--- labs/lab_8/bootstrap.sh
+++ labs/lab_8/bootstrap.sh
@@ -4,6 +4,8 @@ yum update -y
 
 yum install -y git python 3 python3-pip
 
+command -v pip3 || ln -s /usr/bin/pip3 /usr/bin/pip
+
 python 3 -m pip install --upgrade pip
 python 3 -m pip install boto3 pandas requests 
 
